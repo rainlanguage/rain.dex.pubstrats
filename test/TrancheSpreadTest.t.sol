350370d084633bf988aec98164101d2ea8dfbbc6
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-02-28 22:38:00 +0530

    updated tests

--- test/TrancheSpreadTest.t.sol
+++ test/TrancheSpreadTest.t.sol
@@ -8,7 +8,6 @@ import {IInterpreterV2} from "rain.interpreter.interface/interface/unstable/IInt
 import {IInterpreterStoreV2} from "rain.interpreter.interface/interface/unstable/IInterpreterStoreV2.sol";
 import {ISubParserV2} from "rain.interpreter.interface/interface/unstable/ISubParserV2.sol";
 import {IExpressionDeployerV3} from "rain.interpreter.interface/interface/unstable/IExpressionDeployerV3.sol";
-// import {IInterpreterStoreV1} from "rain.interpreter.interface/interface/IInterpreterStoreV1.sol";
 import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 import "src/lib/LibEncodedDispatch.sol";
 import "src/lib/LibNamespace.sol";
@@ -58,9 +57,11 @@ contract TrancheSpreadTest is Test {
         address expression;
         {
             (bytes memory bytecode, uint256[] memory constants) = PARSER.parse(
-                getTrancheSpreadOrder(
+                getTrancheTestSpreadOrder(
                     vm, 
-                    address(ORDERBOOK_SUPARSER)
+                    address(ORDERBOOK_SUPARSER),
+                    0,
+                    101e16
                 )
             );
             (,, expression,) = EXPRESSION_DEPLOYER.deployExpression2(bytecode, constants);
@@ -73,24 +74,33 @@ contract TrancheSpreadTest is Test {
                 new uint256[](0)
         ); 
 
-        uint256 depositedAmount = 390000e18;
-        
-        // Output Tokens offered by order woner
+        uint256 depositedAmount = 400000e18;
+        // Output Tokens offered by order owner
         sellOrderContext[4][4] = depositedAmount;
 
-        // Input Tokens to be received by owner
-        sellOrderContext[3][4] = (calculateStack[0] * depositedAmount)/1e18; 
-
-        console2.log("input : ",sellOrderContext[3][4]);
-        console2.log("output : ",sellOrderContext[4][4]);
-
-        IInterpreterV2(INTERPRETER).eval2(
-                IInterpreterStoreV2(address(STORE)),
-                namespace,
-                LibEncodedDispatch.encode2(expression, SourceIndexV2.wrap(1), type(uint32).max),
-                sellOrderContext,
-                new uint256[](0)
-        );
+        // Below Tranche Limit
+        {
+            sellOrderContext[3][4] = 199e18;
+            vm.expectRevert("Tranche threshold reached.");
+            IInterpreterV2(INTERPRETER).eval2(
+                    IInterpreterStoreV2(address(STORE)),
+                    namespace,
+                    LibEncodedDispatch.encode2(expression, SourceIndexV2.wrap(1), type(uint32).max),
+                    sellOrderContext,
+                    new uint256[](0)
+            );
+        }
+        // Happy Path
+        {
+            sellOrderContext[3][4] = (calculateStack[0] * depositedAmount)/1e18; 
+            IInterpreterV2(INTERPRETER).eval2(
+                    IInterpreterStoreV2(address(STORE)),
+                    namespace,
+                    LibEncodedDispatch.encode2(expression, SourceIndexV2.wrap(1), type(uint32).max),
+                    sellOrderContext,
+                    new uint256[](0)
+            );
+        }
 
     }
 
@@ -100,43 +110,57 @@ contract TrancheSpreadTest is Test {
             LibNamespace.qualifyNamespace(StateNamespace.wrap(uint256(uint160(ORDER_OWNER))), address(this));
 
         uint256[][] memory buyOrderContext = getBuyOrderContext(11223344);
-
-        address expression;
-        {
-            (bytes memory bytecode, uint256[] memory constants) = PARSER.parse(
-                getTrancheSpreadOrder(
-                    vm, 
-                    address(ORDERBOOK_SUPARSER)
-                )
-            );
-            (,, expression,) = EXPRESSION_DEPLOYER.deployExpression2(bytecode, constants);
-        }
-        (uint256[] memory calculateStack,) = IInterpreterV2(INTERPRETER).eval2(
-                IInterpreterStoreV2(address(STORE)),
-                namespace,
-                LibEncodedDispatch.encode2(expression, SourceIndexV2.wrap(0), type(uint32).max),
-                buyOrderContext,
-                new uint256[](0)
-        ); 
-
-        uint256 depositedAmount = 100e18;
-        
+        uint256 depositedAmount = 1000e18;
         // Output Tokens offered by order woner
         buyOrderContext[4][4] = depositedAmount;
-
         // Input Tokens to be received by owner
-        buyOrderContext[3][4] = (calculateStack[0] * depositedAmount)/1e18;
+        buyOrderContext[3][4] = 32700e18; 
 
-        console2.log("input : ",buyOrderContext[3][4]);
-        console2.log("output : ",buyOrderContext[4][4]);
-        
-        IInterpreterV2(INTERPRETER).eval2(
-                IInterpreterStoreV2(address(STORE)),
-                namespace,
-                LibEncodedDispatch.encode2(expression, SourceIndexV2.wrap(1), type(uint32).max),
-                buyOrderContext,
-                new uint256[](0)
-        );
+        address trancheRevert;
+        address trancheExpression;
+
+        {
+            (bytes memory bytecode, uint256[] memory constants) = PARSER.parse(
+                getTrancheTestSpreadOrder(
+                    vm, 
+                    address(ORDERBOOK_SUPARSER),
+                    11e17,
+                    101e16
+                )
+            );
+            (,, trancheRevert,) = EXPRESSION_DEPLOYER.deployExpression2(bytecode, constants);
+        }
+        {
+            (bytes memory bytecode, uint256[] memory constants) = PARSER.parse(
+                getTrancheTestSpreadOrder(
+                    vm, 
+                    address(ORDERBOOK_SUPARSER),
+                    12e17,
+                    101e16
+                )
+            );
+            (,, trancheExpression,) = EXPRESSION_DEPLOYER.deployExpression2(bytecode, constants);
+        }
+        {
+            vm.expectRevert("Tranche threshold reached.");
+            IInterpreterV2(INTERPRETER).eval2(
+                    IInterpreterStoreV2(address(STORE)),
+                    namespace,
+                    LibEncodedDispatch.encode2(trancheRevert, SourceIndexV2.wrap(1), type(uint32).max),
+                    buyOrderContext,
+                    new uint256[](0)
+            );
+        }
+        {
+            IInterpreterV2(INTERPRETER).eval2(
+                    IInterpreterStoreV2(address(STORE)),
+                    namespace,
+                    LibEncodedDispatch.encode2(trancheExpression, SourceIndexV2.wrap(1), type(uint32).max),
+                    buyOrderContext,
+                    new uint256[](0)
+            );
+        }
+         
 
     }
 
@@ -177,6 +201,52 @@ contract TrancheSpreadTest is Test {
         ffi[26] = string.concat("spread-ratio=", uint2str(101e16));
         ffi[27] = "--bind";
         ffi[28] = string.concat("tranche-space-edge-guard-threshold=", uint2str(2e17));
+        
+        
+        trancheRefill = bytes.concat(getSubparserPrelude(orderBookSubparser), vm.ffi(ffi));
+    }
+
+    function getTrancheTestSpreadOrder(
+        Vm vm,
+        address orderBookSubparser,
+        uint256 testTrancheSpace,
+        uint256 spreadRatio
+    )
+        internal
+        returns (bytes memory trancheRefill)
+    {
+        string[] memory ffi = new string[](31);
+        ffi[0] = "rain";
+        ffi[1] = "dotrain";
+        ffi[2] = "compose";
+        ffi[3] = "-i";
+        ffi[4] = "src/tranche-spread.rain";
+        ffi[5] = "--entrypoint";
+        ffi[6] = "calculate-io";
+        ffi[7] = "--entrypoint";
+        ffi[8] = "handle-io";
+        ffi[9] = "--bind";
+        ffi[10] = "distribution-token=0xd0e9c8f5Fae381459cf07Ec506C1d2896E8b5df6";
+        ffi[11] = "--bind";
+        ffi[12] = "reserve-token=0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
+        ffi[13] = "--bind";
+        ffi[14] = "get-tranche-space='get-test-tranche-space";
+        ffi[15] = "--bind";
+        ffi[16] = "set-tranche-space='set-test-tranche-space";
+        ffi[17] = "--bind";
+        ffi[18] = string.concat("test-tranche-space=", uint2str(testTrancheSpace));
+        ffi[19] = "--bind";
+        ffi[20] = "tranche-reserve-amount-growth='tranche-reserve-amount-growth-constant";
+        ffi[21] = "--bind";
+        ffi[22] = string.concat("tranche-reserve-amount-base=", uint2str(1000e18));
+        ffi[23] = "--bind";
+        ffi[24] = "tranche-reserve-io-ratio-growth='tranche-reserve-io-ratio-linear";
+        ffi[25] = "--bind";
+        ffi[26] = string.concat("tranche-reserve-io-ratio-base=", uint2str(327e18));
+        ffi[27] = "--bind";
+        ffi[28] = string.concat("spread-ratio=", uint2str(spreadRatio));
+        ffi[29] = "--bind";
+        ffi[30] = string.concat("tranche-space-edge-guard-threshold=", uint2str(2e17));
         
         
         trancheRefill = bytes.concat(getSubparserPrelude(orderBookSubparser), vm.ffi(ffi));
