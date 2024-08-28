ee5dc75df0755a529f9bcd8a62e1e96160575290
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-08-28 10:51:38 +0530

    add CI task

--- flake.nix
+++ flake.nix
@@ -2,17 +2,41 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    rainix.url = "github:rainprotocol/rainix";
     flake-utils.url = "github:numtide/flake-utils";
+    rainix.url = "github:rainprotocol/rainix";
   };
 
-  outputs = {self, flake-utils, rainix }:
+  outputs = {self, rainix, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = rainix.pkgs.${system};
-      in {
-        packages = rainix.packages.${system};
-        devShells = rainix.devShells.${system};
+      in rec {
+        packages = rec{
+          network-list = rainix.network-list.${system};
+          networks = pkgs.lib.concatStringsSep " " network-list;
+
+          check-deployer-words = rainix.mkTask.${system} {
+            name = "check-deployer-words";
+            body = ''
+              for network in ${networks}
+              do
+                echo "Checking deployer words for $network"
+                cargo run --manifest-path ''${MANIFEST_PATH} --package rain_orderbook_cli words -c ''${SETTINGS_PATH} -d "$network" --stdout
+              done
+            '';
+          };
+        } // rainix.packages.${system};
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            packages.check-deployer-words
+          ];
+
+          shellHook = rainix.devShells.${system}.default.shellHook;
+          buildInputs = rainix.devShells.${system}.default.buildInputs;
+          nativeBuildInputs = rainix.devShells.${system}.default.nativeBuildInputs;
+        };
+
       }
     );
 
