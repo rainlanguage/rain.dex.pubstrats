fbc112440bfcfdaf901c9ba713d2494ffa114236
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-09-05 18:36:40 +0530

    update flake

--- flake.nix
+++ flake.nix
@@ -19,12 +19,34 @@
             name = "check-deployer-words";
             body = ''
               set -euxo pipefail
-              
-              for network in ${networks}
-              do
-                echo "Checking deployer words for $network"
-                cargo run --manifest-path ''${MANIFEST_PATH} --package rain_orderbook_cli words -c ''${SETTINGS_PATH} -d "$network" --stdout
+
+              dotrain_folder_path=''${DOTRAIN_FOLDER_PATH:-.}
+
+              dotrain_paths=""
+              for folder in "$dotrain_folder_path"/*; do
+                  if [ -d "$folder" ]; then
+                      dotrain=$(find "$folder" -type f -name "*.rain" -print)
+                      dotrain_paths+=" $dotrain"
+                  fi
               done
+
+              for dotrain in "$dotrain_folder_path"/*.rain ; do
+                  dotrain_paths+=" $dotrain"
+              done
+
+              dotrain_paths=$(echo "$dotrain_paths" | tr -s ' ' | sed 's/^ //;s/ $//')
+
+              for dotrain in $dotrain_paths
+              do
+                echo "Checking deployments within $dotrain"
+                deployment_keys=$(cargo run --manifest-path ''${MANIFEST_PATH} --package rain_orderbook_cli order keys -f $dotrain ''${SETTINGS_PATH:+-c "''${SETTINGS_PATH}"})
+                for key in $deployment_keys
+                do
+                  echo "key: $key"
+                  cargo run --manifest-path ''${MANIFEST_PATH} --package rain_orderbook_cli words -f $dotrain ''${SETTINGS_PATH:+-c "''${SETTINGS_PATH}"} --deployment "$key" --stdout
+                done
+              done
+              
             '';
           };
         } // rainix.packages.${system};
